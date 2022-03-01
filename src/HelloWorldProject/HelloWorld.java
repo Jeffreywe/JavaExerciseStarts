a041db6a8352041094168325c5bd4e382ce9cd7d
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-03-01 10:34:50 -0500

    Initial commit

---
+++ src/HelloWorldProject/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorldProject;
+
+public class HelloWorld {
+
+	/**
+	 * This is the main method, that runs the program.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+
+}
